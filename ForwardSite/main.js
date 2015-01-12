c8417e6b120c4fc03f5298f3bb7f591e332acadf
ryanprimeau <ryanpatrickprimeau@gmail.com>
2015-01-12 12:55:52 -0500

    First commit

---
+++ ForwardSite/main.js
@@ -0,0 +1,5 @@
+var element = document.querySelector("#fullname");
+element.innerText = "Ryan Primeau";
+var element = document.querySelector("#email");
+element.innerText = "ryanprimeau@forward.com";
+console.log("Hi")
